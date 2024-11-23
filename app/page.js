16b7542503dbe4a62cd2b35d31b296d3adf989cf
Sandeepon260 <sandeepon.saha@gmail.com>
2024-11-23 16:53:41 -0500

    alert(file)

--- app/page.js
+++ app/page.js
@@ -8,7 +8,10 @@ export default function Home() {
     e.preventDefault();
     const formData = new FormData();
     const file = e.target.file.files[0];
-    formData.append('file', file);
+    formData.append('help me', file);
+    const keys = formData.keys();
+    alert(keys.next().value);
+    alert(file.name)
 
     const res = await fetch('/api/upload', {
       method: 'POST',
@@ -26,10 +29,13 @@ export default function Home() {
   return (
     <div>
       <h1>Upload Image</h1>
-      <form onSubmit={handleUpload}>
+      
+      <form onSubmit={handleUpload} >
         <input type="file" name="file" accept=".jpg,.png" />
         <button type="submit">Upload</button>
       </form>
+
+      
       {imageUrl && (
         <div>
           <h2>Uploaded Image:</h2>
